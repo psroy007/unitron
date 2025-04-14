3959a4d20fb7fdc54e684c94692416bd17833116
Purab Singha Roy <91241498+psroy007@users.noreply.github.com>
2025-04-15 03:40:24 +0530

    Update loading-screen.tsx

--- components/loading-screen.tsx
+++ components/loading-screen.tsx
@@ -3,8 +3,17 @@
 export default function LoadingScreen() {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black">
+      {/* Video for desktop view */}
       <video
-        className="w-auto h-auto max-w-full max-h-full object-cover sm:scale-100 scale-125" // Adjusted scaling and added constraints
+        className="hidden sm:block w-auto h-auto max-w-full max-h-full object-cover sm:scale-100"
+        src="/videos/intro3.mp4"
+        autoPlay
+        loop
+        muted
+      />
+      {/* Video for mobile view */}
+      <video
+        className="block sm:hidden w-auto h-auto max-w-full max-h-full object-cover scale-125"
         src="/videos/loading-mobile.mp4"
         autoPlay
         loop
