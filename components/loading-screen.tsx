7eeab2b4e964b7a595e6a1fe4f6200e859a938f9
Purab Singha Roy <91241498+psroy007@users.noreply.github.com>
2025-04-15 03:27:46 +0530

    Update loading-screen.tsx

--- components/loading-screen.tsx
+++ components/loading-screen.tsx
@@ -5,7 +5,7 @@ export default function LoadingScreen() {
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black">
       <video
         className="w-auto h-auto max-w-full max-h-full object-cover sm:scale-100 scale-125" // Adjusted scaling and added constraints
-        src="/videos/intro3.mp4"
+        src="/videos/loading-mobile.mp4"
         autoPlay
         loop
         muted
