bf6434151e060b42dbcc602e616fdff9309805e2
Purab Singha Roy <91241498+psroy007@users.noreply.github.com>
2025-04-16 22:59:43 +0530

    Update loading-screen.tsx

--- components/loading-screen.tsx
+++ components/loading-screen.tsx
@@ -15,7 +15,7 @@ export default function LoadingScreen() {
       {/* Video for mobile view */}
       <video
         className="block sm:hidden w-auto h-auto max-w-full max-h-full object-cover scale-125"
-        src="/videos/spider_load.mp4"
+        src="/videos/loading_vishal.mp4"
         autoPlay
         loop
         muted
